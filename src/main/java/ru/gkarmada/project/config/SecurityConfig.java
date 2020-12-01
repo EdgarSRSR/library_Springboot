724f72a0b67cef85da2b25256690b223e3104705
Al Zakharov <a.zakharov@gkarmada.ru>
2020-12-01 19:57:59 +0700

    little fixes to

--- src/main/java/ru/gkarmada/project/config/SecurityConfig.java
+++ src/main/java/ru/gkarmada/project/config/SecurityConfig.java
@@ -79,7 +79,7 @@ class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
             .antMatchers("/library*").hasAnyRole("user", "admin")
             .antMatchers("/genre*").hasAnyRole("user", "admin")
             .antMatchers("/author*").hasAnyRole("user", "admin")
-            .anyRequest().permitAll(),
+            .anyRequest().permitAll();
     }
 
 }
